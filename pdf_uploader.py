c34b2140ae3c7212a276645fc4cbc2236f1586d6
MrH-SWES <whenderson@sw.wednet.edu>
2025-03-28 09:07:01 -0700

    Update pdf_uploader.py

--- pdf_uploader.py
+++ pdf_uploader.py
@@ -4,7 +4,7 @@ import time
 import tempfile
 import re
 import traceback
-from langchain.document_loaders import PyPDFLoader
+from langchain_community.document_loaders import PyPDFLoader
 from langchain.text_splitter import RecursiveCharacterTextSplitter
 from langchain_openai import OpenAIEmbeddings
 from langchain_pinecone import PineconeVectorStore
